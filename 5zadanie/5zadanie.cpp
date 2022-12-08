80347e030c3b37916f56f9b0d926d93682187fc6
revelations? <flfkflfkkf@gmail.com>
2022-12-08 11:34:32 +0200

    Добавьте файлы проекта.

---
+++ 5zadanie/5zadanie.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, ".1251");
+	double x, y, q;
+	cout << "Введіть значення x=";
+	cin >> x;
+	if (x <= -2){
+		cout << "вираз не можете бути менше -2;(";
+		return 0;}
+	cout << "Введіть значення y=";
+	cin >> y;
+	q = sqrt(sqrt((x + 2) / (3*y + 1)) + (8*x * x + 3) / (1 - 9*y * y));
+	cout << "Значення q=" << q << endl;
+	system("pause>>void");
+	return 0;
+}
